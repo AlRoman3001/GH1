812c9b677878e53529dd7e4f8b6f62cd623fd55f
AlRoman3001 <91065192+AlRoman3001@users.noreply.github.com>
2021-09-21 14:49:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'привет')
+    print(f'привет1111111')
     print('ура')
 
 # Press the green button in the gutter to run the script.
